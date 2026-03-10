a0f9189b4511b8794f0f76fddb53a507fc033d83
CoveredTechSis <101675142+CoveredTechSis@users.noreply.github.com>
2026-03-10 03:33:57 -0700

    latest changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
+import { fileURLToPath } from "url"
+
+// Manually define __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/PrideAdvisoryHub/",
+ // base: "/PrideAdvisoryHub/",
   plugins: [react()],
-  resolve: {
+ resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "../shared"),
+      "@assets": path.resolve(__dirname, "../../attached_assets"),
     },
   },
+
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
   build: {
     outDir: "dist",
     emptyOutDir: true,
