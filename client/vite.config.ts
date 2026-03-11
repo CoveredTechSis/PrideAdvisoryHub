e930da81a27418700eb40dc996ae3e30d7c4d774
CoveredTechSis <101675142+CoveredTechSis@users.noreply.github.com>
2026-03-11 03:33:22 -0700

    latest path changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+ import react from "@vitejs/plugin-react"
+ import path from "path"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
+ 
+ export default defineConfig({
+   plugins: [react()],
+   resolve: {
     alias: {
       "@": path.resolve(process.cwd(), "src"),
-    },
+      "@assets": path.resolve(process.cwd(), "attached_assets"),
+     },
   },
   build: {
     outDir: "dist",
-    emptyOutDir: true,
-  },
-})
-
+     emptyOutDir: true,
+   },
+ })
 
 
 
